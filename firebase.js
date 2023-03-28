bf48db12f66ef3b364ab3605d476fe7b75ca2809
Veera Srivastava <veerasriv@gmail.com>
2023-03-28 14:05:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,26 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCfRLAa14RrN6QQZNFOaNS3VEf_jGd6xEA",
-    authDomain: "nextjsblog-50c9f.firebaseapp.com",
-    projectId: "nextjsblog-50c9f",
-    storageBucket: "nextjsblog-50c9f.appspot.com",
-    messagingSenderId: "968067246305",
-    appId: "1:968067246305:web:1fc61dad716ece73a1afa0"
-  };
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAIfpudEtk7LHj6LzrklsE9c4BomRytzJU",
+
+  authDomain: "blog-2ba4f.firebaseapp.com",
+
+  databaseURL: "https://blog-2ba4f-default-rtdb.firebaseio.com",
+
+  projectId: "blog-2ba4f",
+
+  storageBucket: "blog-2ba4f.appspot.com",
+
+  messagingSenderId: "24919826708",
+
+  appId: "1:24919826708:web:0cc5fbf19b9c30e343a201",
+
+  measurementId: "G-THK94Z0MB4"
+
+};
+
 
 
 if(!firebase.apps.length) firebase.initializeApp(firebaseConfig)
